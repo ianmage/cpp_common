b8d0b04a4eae8433d5b98f4eb76b2567428bb0b0
iAn <iAn@iAntekiMacBook-Pro.local>
2018-07-09 03:18:42 +0800

    more helper func

--- containerUtil.h
+++ containerUtil.h
@@ -1,2 +1,14 @@
 #pragma once
 
+#include <functional>
+
+
+struct PairHash
+{
+	template <typename T1, typename T2>
+	std::size_t operator()(std::pair<T1,T2> const & p) {
+		auto h1 = std::hash<T1>{}(p.first);
+		auto h2 = std::hash<T2>{}(p.second);
+		return h1 ^ h2;
+	}
+};
