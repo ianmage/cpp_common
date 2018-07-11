d5a03b90c85304626ee1c72583fcc9b84e787a6a
ian <huangyien@corp.netease.com>
2018-07-11 23:03:25 +0800

    simplify str func decl

--- strUtil.h
+++ strUtil.h
@@ -3,14 +3,14 @@
 #include <string>
 
 
-bool startsWith(std::string const & mainStr, std::string const & toMatch)
+inline bool startsWith(std::string const & mainStr, std::string const & toMatch)
 {
 	if (mainStr.length() >= toMatch.length())
 		return 0 == mainStr.compare(0, toMatch.length(), toMatch);
 	return false;
 }
 
-bool endsWith(std::string const & mainStr, std::string const & toMatch)
+inline bool endsWith(std::string const & mainStr, std::string const & toMatch)
 {
 	if (mainStr.length() >= toMatch.length())
 		return 0 == mainStr.compare(mainStr.length() - toMatch.length(), toMatch.length(), toMatch);
@@ -18,9 +18,8 @@ bool endsWith(std::string const & mainStr, std::string const & toMatch)
 }
 
 
-void replace(std::string & src, std::string const & find, std::string const & rep)
+inline void replace(std::string & src, std::string const & find, std::string const & rep)
 {
 	for (size_t i = 0; (i = src.find(find, i)) != std::string::npos; i += rep.length())
 		src.replace(i, find.length(), rep);
 }
-
