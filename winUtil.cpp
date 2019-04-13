a8e0b413d199dcccd4184a0d7ab5fa598c8e3a2a
ianmage <ian.wong@163.com>
2019-04-13 20:05:22 +0800

    differentiate char / wchar version

--- winUtil.cpp
+++ winUtil.cpp
@@ -8,7 +8,14 @@
 
 void ConsoleWriteLn(char const * s, size_t len)
 {
+#ifdef UNICODE
 	std::wstring wc(len + 1, L'\n');
 	mbstowcs(&wc[0], s, len);
 	::OutputDebugString(wc.c_str());
+#else
+	std::string c(len + 1, L'\n');
+	strcpy(&c[0], s);
+	c[len] = '\n';
+	::OutputDebugString(c.c_str());
+#endif
 }
